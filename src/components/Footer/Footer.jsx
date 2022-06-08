1dfeca732bc3de92c4dbf20f39d93bde46fef4bb
Tolya Fam <famovkin@yandex.ru>
2022-06-08 21:31:36 +0300

    feat: create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from '../../images/logo.svg';
+
+import './Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <a href="https://onpoint.ru" target="_blank" rel="noreferrer">
+      <img className="footer__logo" src={logo} alt="Onpoint" />
+    </a>
+  </div>
+);
+
+export default Footer;
