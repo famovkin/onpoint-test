42e73c4f01b0cfa62222bf53b99344c1cb54aada
Tolya Fam <famovkin@yandex.ru>
2022-06-09 19:48:15 +0300

    refactor: add icon props for Button

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import arrow from '../../../images/right-arrow.svg';
-
 import './Button.css';
 
-const Button = ({ children, handler, isAbsolute }) => (
+const Button = ({
+  children,
+  handler,
+  isAbsolute,
+  icon,
+}) => (
   <button className={`button ${isAbsolute ? 'button_type_absolute' : ''}`} onClick={handler}>
     <div className="button__arrow-box">
-      <img className="button__arrow" src={arrow} alt="Right arrow" />
+      <img className="button__arrow" src={icon} alt="Right arrow" />
     </div>
     <span className="button__text">
       {children}
